a6a33501e774126097355b430bb7431687f52b12
johnwoodill <johnwoodill@gmail.com>
2022-04-13 07:07:06 -0700

    path

--- scripts/plot_climatology.py
+++ scripts/plot_climatology.py
@@ -14,7 +14,7 @@ today = date.today()
 path = "/tmp/nj_oradell_reservoir_dashboard/proc_dashboard_data/"
 pwd = path+"now_cast_tab/"
 #data = pd.read_csv(pwd + "or_detroit_lake_nowcast_expected_longrun_predictions.csv",parse_dates=["date"])
-data = pd.read_csv(pwd + "nj_oradell_reservoir_nowcast_expected_longrun_predictions.csv",parse_dates=["date"])
+data = pd.read_csv(pwd + "nj_oradell_reservoir_nowcast_longrun_predictions.csv",parse_dates=["date"])
 
 
 data["month"] = data["date"].dt.month
